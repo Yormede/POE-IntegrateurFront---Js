5e47b1b66649bc007a6477bbbb84fa03d3b9485f
Yormede <ahmedsvg69@gmail.com>
2024-04-25 11:18:17 +0200

    class creation

---
+++ js/poo-intro.js
@@ -0,0 +1,13 @@
+class Voiture(marque, modele) {
+    this._marque = marque;
+    this._modele = modele;
+    this._vitesse = null;
+    this._options = [];
+    
+    get getModele() {
+        return this._modele;
+    }
+
+
+}
+

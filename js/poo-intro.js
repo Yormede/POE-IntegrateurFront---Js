bf134e7cd57edf21e366d5db8ed7f6d1b596ce8a
Yormede <ahmedsvg69@gmail.com>
2024-04-25 11:26:25 +0200

    Avancement sur la class

--- js/poo-intro.js
+++ js/poo-intro.js
@@ -1,13 +1,30 @@
-class Voiture(marque, modele) {
-    this._marque = marque;
-    this._modele = modele;
-    this._vitesse = null;
-    this._options = [];
-    
-    get getModele() {
-        return this._modele;
+class Voiture {
+    constructor(marque, modele) {
+        this._marque = marque;
+        this._modele = modele;
+        this._vitesse = null;
+        this._options = [];
+        
+        get getModele() {
+            return this._modele;
+        };
+        get getMarque() {
+            return this._marque;
+        };
+        get getVitesse() {
+            return this._vitesse;
+        };
+        get getOptions() {
+            return this._options;
+        };
     }
-
-
 }
 
+const firstGov = new Voiture("Ford", "Mustang");
+
+console.log(firstGov);
+console.log(firstGov.constructor.name);
+console.log(firstGov._marque);
+console.log(firstGov._modele);
+// console.log();
+// console.log();
